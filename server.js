e91f866173b1a6293d3e24639a4553c5e1051a55
shuubhambadiwal <shubhambadiwal2@gmail.com>
2024-12-03 03:40:32 +0530

    Upadted the depndcs and chnage the port of the client

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
-
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 mongoose.connect(
-    'mongodb+srv://shubhambadiwal2:Wealth@335566@cluster2.josdk.mongodb.net/'
-).then(()=>console.log('MongoDB connected')).catch(error => console.log(error));
+    'mongodb+srv://shubhambadiwal2:Wp07WDklkovSQ49v@cluster2.josdk.mongodb.net/'
+).then(()=>console.log('MongoDB connected'))
+.catch(error => console.log(error));
 
 const app =  express ();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+
+app.use(
+    cors({
+        origin : 'http://localhost:5173',
+        methods : ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders : [
+            "Content-Type",
+            'Authorization',
+            'Cache-Control',
+            'Expires',
+            'Pragma'
+         ],
+        credentials : true
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, ()=> console.log(`Server is now running on the port ${PORT}`));
