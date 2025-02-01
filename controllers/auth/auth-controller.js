4d034efae05a89bf6c242cc81c72d572ecc9ad3c
shuubhambadiwal <shubhambadiwal2@gmail.com>
2025-02-01 13:47:41 +0530

    building auth

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -4,7 +4,15 @@ import User from '../../models/User.js';
 
 export const registerUser = async (req, res) => {
     const { userName, email, password } = req.body;
+    //register logic here
     try {
+        const checkUser = await User.findOne({ email });
+        if (checkUser) {
+            return res.json({
+                success: false,
+                message: "User already exists with the same email"
+            });
+        }
         const hashedPassword = await bcrypt.hash(password, 12);
         const newUser = new User({
             userName, email, password: hashedPassword
@@ -24,9 +32,26 @@ export const registerUser = async (req, res) => {
     }
 };
 
-export const login = async (req, res) => {
-    const { userName, email, password } = req.body;
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
     try {
+        const checkUser = await User.findOne({ email });
+        if(!checkUser) return res.json({ success: false, message: "User does not exists"  });
+        
+        const checkPasswordMatch = await bcrypt.compare(password, checkUser.password);
+        if (!checkPasswordMatch) return res.json({ success: false, message: "Invalid Password"});
+
+        const token = jwt.sign({id: checkUser._id, role : checkUser.role, email: checkUser.email },'CLIENT_SECRET_KEY',{expiresIn : '60m'});
+        
+        res.cookie('token', token, { httpOnly: true, secure : false }).json({
+            success: true,
+            message: "Login successful",
+            user : {
+                email : checkUser.email,
+                role : checkUser.role,
+                id : checkUser._id
+            },
+        });
         // login logic here
     } catch (e) {
         console.log(e);
