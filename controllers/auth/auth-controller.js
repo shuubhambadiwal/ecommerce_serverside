7be7ae42688b10983ea0df5d2ce06a64116ada1e
shuubhambadiwal <shubhambadiwal2@gmail.com>
2025-02-04 14:54:33 +0530

    add chekc auth middleware and and routes for checkauth

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../../models/User.js';
 
+// For register user
 export const registerUser = async (req, res) => {
     const { userName, email, password } = req.body;
     //register logic here
@@ -32,6 +33,7 @@ export const registerUser = async (req, res) => {
     }
 };
 
+// For login user
 export const loginUser = async (req, res) => {
     const { email, password } = req.body;
     try {
@@ -60,4 +62,35 @@ export const loginUser = async (req, res) => {
             message: "Some error occurred"
         });
     }
+};
+
+// For logout user
+
+export const logoutUser = async (req, res) => {
+    res.clearCookie('token').json({
+        success: true,
+        message: "Logout successful"
+    });
+};
+
+
+//Auth controller
+
+export const authMiddleware = async (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) 
+        return res.status(401).json({
+            success: false,
+            message: "User not authenticated"
+        }) 
+        try {
+            const decoded = jwt.verify(token, 'CLIENT_SECRET_KEY');
+            req.user = decoded;
+            next();
+        } catch (error) {
+            return res.status(401).json({
+                success: false,
+                message: "User not authenticated"
+            });
+        }  
 };
--- routes/auth/auth-routes.js
+++ routes/auth/auth-routes.js
@@ -1,9 +1,18 @@
 import express from 'express';
-import { registerUser, loginUser } from '../../controllers/auth/auth-controller.js';
+import { registerUser, loginUser, logoutUser, authMiddleware } from '../../controllers/auth/auth-controller.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/logout', logoutUser);
+router.get('/check-auth', authMiddleware, (req, res) => {
+    const user = req.user;
+    res.status(200).json({
+      success: true,
+      message: 'User authenticated',
+      user
+    })
+})
 
 export default router;
